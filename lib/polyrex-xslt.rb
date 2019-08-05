17147f4893b6be003a5f79613c87fa874a3c6fe5
James Robertson <james@r0bertson.co.uk>
2019-08-05 08:37:24 +0100

    rewrite: This gem now converts a nested XML structure into a Polyrex XSLT document suitable for generating a Polyrex document.

--- lib/polyrex-xslt.rb
+++ lib/polyrex-xslt.rb
@@ -2,89 +2,101 @@
 
 # file: polyrex-xslt.rb
 
-class PolyrexXSLT
-  
-  attr_accessor :schema, :xslt_schema
+require 'rexle'
+require 'rexle-builder'
 
-  def initialize(options={})
-    o = {schema: '', xslt_schema: ''}.merge(options)
-    @schema, @xslt_schema = o.values
+
+class PolyrexXSLT
+
+  def initialize(schema: '', xslt_schema: '', debug: false)
+
+    @schema, @xslt_schema, @debug = schema, xslt_schema, debug
+
   end
 
   def to_xslt()
 
-header =<<HEADER
-<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-<xsl:output method="xml" indent="yes" />
-
-HEADER
-
     a_element = @schema.split('/').map{|x| x[/\w+/]}
 
+    @xslt_schema = build_xslt_schema(@schema) if @xslt_schema.empty?
+    puts ('@xslt_schema: ' + @xslt_schema.inspect).debug if @debug
+
     a_html = @xslt_schema.split('/').map do |x|
-     
-      name = x[/\w+/]
-      children = ($')[/^\[([^\]]+)\]$/,1]
-      list = children.split(',').map {|y| y.split(':') } if children
+
+      result = x.match(/([\w\>]+)(?:[\(\[]([^\]\)]+)[\]\)])?(.*)/)
+      name, children, remaining = result.captures if result
+
+      list = children.split(/ *, */).map {|y| y.split(':',2)} if children        
 
       [name, list]
     end
 
-    a = a_element.zip(a_html).map.with_index do |a,i|
-      out = []
-      tag = a.shift
-      field = i > 0 ? 'records/' + tag : tag
-      out << "<xsl:template match='#{field}'>" + "\n"
-      a.flatten!(1)
-      if a.last.is_a? Array then
+    puts ('a_html: ' + a_html.inspect).debug if @debug
 
-        out << scan_e(a, tag) 
-        out << "</xsl:template>\n\n"
-      else
-        out << "  <%s>\n" % a.first
-        out << "    <xsl:apply-templates select='summary'/>\n"
-        out << "    <xsl:apply-templates select='records'/>\n"
-        out << "  <\\%s>\n" % a.first
-        out << "</xsl:template>\n\n"
-        out << "<xsl:template match='%s/summary'>\n" % [tag]
-        out << "</xsl:template>\n\n"
+    a = a_element.zip(a_html)
+
+    xml = RexleBuilder.new
+    raw_a = xml.xsl_stylesheet(xmlns_xsl: \
+                     "http://www.w3.org/1999/XSL/Transform", version: "1.0") do
+      xml.xsl_output(method: "xml", indent: "yes", \
+                                              :"omit-xml-declaration" => "yes")
+
+      a.each_cons(2).with_index do |x,i|
+
+        x1, x2 =  x
+        puts 'x1: '  + x1.inspect if @debug
+        build(xml, x1, x2)
+        build(xml, x2) if x2
+
       end
-      
-      out
+
     end
 
-    header + a.flatten.join + "</xsl:stylesheet>"
+    xml2 = Rexle.new(raw_a).xml(pretty: true).gsub('xsl_apply_templates',\
+        'xsl:apply-templates').gsub('xsl_value_of','xsl:value-of').\
+        gsub('xsl_template','xsl:template').gsub('xsl_','xsl:').\
+        gsub('xmlns_xsl','xmlns:xsl')
+    
+header = %q(
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:output method="xml" indent="yes" />
+)
+
+    header + xml2
+    
   end
 
   private
 
-  def scan_e(a, prev_tag='', indent='  ')
+  def build(xml, a1, a2=nil)
 
-    out = []
+    xml.xsl_template(match: a1[1][0]) do
 
-    unless a.first.is_a? Array then
-      tag = a.shift
-      out << indent + "<%s>\n" % tag
-      out << indent + "  <xsl:apply-templates select='summary'/>\n"
-      out << indent + "<\\%s>\n" % tag
-      out << "</xsl:template>\n\n"
-      out << "<xsl:template match='%s/summary'>\n" % [prev_tag]
+      xml.send a1[0].to_sym do
 
-      a.flatten!(1)
-      if a.last.is_a? Array then
-        out << scan_e(a, tag, indent + '  ') 
-      else
-        out << indent + '  ' + a.first + "\n"
-      end
+        xml.summary do
 
-    else
-      a.map do |x| 
-        out << indent + "<%s><xsl:value-of select='%s'/></%s>\n" % [x[0],x[-1],x[0]]
-      end
+          if a1[1][1] then
+
+            a1[1][1].each do |source, target|
+            
+              xml.xsl_element(name: target) do
+                xml.xsl_value_of(select: source)  
+              end
+
+            end
+
+          end
+
+        end        
+
+        xml.records do
+          xml.xsl_apply_templates(select:  a2[1][0]) if a2
+        end
+      end 
     end
 
-    out
   end
-  
+
 end
--- polyrex-xslt.gemspec
+++ polyrex-xslt.gemspec
@@ -1,12 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'polyrex-xslt'
-  s.version = '0.1.2'
-  s.summary = 'polyrex-xslt'
+  s.version = '0.2.0'
+  s.summary = 'Generates XSLT to produce a Polyrex document from a tree-like XML structure.'
     s.authors = ['James Robertson']
-  s.files = Dir['lib/**/*.rb'] 
+  s.files = Dir['lib/polyrex-xslt.rb'] 
+  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.2')  
   s.signing_key = '../privatekeys/polyrex-xslt.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'james@jamesrobertson.eu'
   s.homepage = 'https://github.com/jrobertson/polyrex-xslt'
+  s.required_ruby_version = '>= 2.1.2'
 end
